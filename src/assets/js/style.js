a59737ee34fb1792df498836c1a538cd1458284d
Singgih Saputro <singgihsap@gmail.com>
2020-06-02 09:56:26 +0700

    MCF Development

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -2,27 +2,111 @@ $(document).ready(function () {
     /*========================================*/
     /* START: Slide menu */
     /*========================================*/
-    $("#menu-toggle").click(function(e) {
-        console.log('ini toggle');
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
+    $("#openNav").click(function () {
+      $("#menuSidenav").addClass("show");
+    });
+    $("#closeNav").click(function () {
+      $("#menuSidenav").removeClass("show");
     });
 
     /*========================================*/
-    /* START: SCROLL NAVBAR */
+    /* START: Mansory Grid */
     /*========================================*/
-    var header = $(".navbar--transparent");
-    var button = $(".btn--transparent");
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-
-        if (scroll >= 10) {
-            header.removeClass('navbar--transparent').addClass('navbar--home');
-            button.removeClass('btn--transparent').addClass('btn--transparent-black');
-        } else {
-            header.removeClass('navbar--home').addClass('navbar--transparent');
-            button.removeClass('btn--transparent-black').addClass('btn--transparent');
-        }
+    $(".grid").masonry({
+      // options
+      itemSelector: ".grid-item",
+      columnWidth: 145,
+      fitWidth: true,
+      gutter: 12,
     });
 
+    /*========================================*/
+    /* START: Search Mobile */
+    /*========================================*/
+    // Handling for Search Mobile
+    $(".search-input-mobile").on("keyup", function () {
+      if ($(".search-input-mobile").val().length >= 3) {
+        $(".box__result").addClass("visible");
+      } else if ($(".search-input").val().length === 0) {
+        $(".box__result").removeClass("visible");
+      }
+    });
+    $(".toggle-search-mobile").click(function () {
+        $(".search-box--mobile").addClass("visible");
+    });
+    $(".close-search-mobile").click(function () {
+        $(".search-box--mobile").removeClass("visible");
+    });
+
+    /*========================================*/
+    /* START: Slick Slider */
+    /*========================================*/
+    // Slider Position
+    $(".slider--position").slick({
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1150,
+          settings: {
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+
+    // Slider Year
+    $(".slider--year").slick({
+      infinite: false,
+      speed: 200,
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 1150,
+          settings: {
+            infinite: false,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            infinite: false,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
 });
